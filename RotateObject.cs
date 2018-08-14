349779f059a49aa7886a344a5639c5ab0a99f614
maco1028 <maco1028@gmail.com>
2018-08-15 03:35:32 +0900

    Initial commit

---
+++ RotateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+	public float RY=45;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		//変数で回転させる
+		transform.Rotate(0, RY * Time.deltaTime, 0);
+	}
+}
