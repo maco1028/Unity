43af6db8808e701aed9d8f65098aec72216efc69
maco1028 <maco1028@gmail.com>
2018-08-15 08:49:58 +0900

    maco

---
+++ LeftRightMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRightMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(Mathf.PingPong(Time.time,18)-10, transform.position.y, transform.position.z);
+	}
+}
