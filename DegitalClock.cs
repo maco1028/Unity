24a4c79df642702ead8f9f4fbd80b3d2428bd479
maco1028 <maco1028@gmail.com>
2018-08-15 10:44:21 +0900

    maco

--- DegitalClock.cs
+++ DegitalClock.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 using System;
 
+//時計のプログラム
 public class DegitalClock : MonoBehaviour {
 
 	public Text degtime;
@@ -12,13 +13,13 @@ public class DegitalClock : MonoBehaviour {
 		degtime = GetComponent<Text>() as Text;
 	}
 	
-	// Update is called once per frame
+	//日付と時間を取得
 	void Update () {
 		DateTime nowtime =DateTime.Now;
 		hour = nowtime.Hour;
 		min = nowtime.Minute;
 		sec = nowtime.Second;
-
+	//時間を表示
 		degtime.text = hour.ToString("00")+":"+min.ToString("00")+":"+sec.ToString("00");
 	}
 }
